7df3a57a0c86a21a52466f4be4ffa50369593034
alissao <alissonhbv@gmail.com>
2020-03-18 16:47:20 -0300

    18/03

---
+++ src/br/com/cotefacil/cliente/ClienteWebService.java
@@ -0,0 +1,25 @@
+package br.com.cotefacil.cliente;
+
+import java.io.IOException;
+
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.fluent.Form;
+import org.apache.http.client.fluent.Request;
+
+public class ClienteWebService {
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		String conteudo = Request
+			.Post("http://localhost:8080/gerenciador/Empresas")
+			.addHeader("accept", "application/json")
+		    .execute()
+		    .returnContent()
+		    .asString();
+		
+		System.out.println(conteudo);
+
+	}
+
+}
